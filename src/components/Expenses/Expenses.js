5b7fb918447db49124895b214147beea8505b673
Trian Afiansyah <trianafiansyah@bagidata.com>
2021-06-19 12:38:24 +0700

    first commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter'
+import Card from '../UI/Card';
+
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2021')
+
+  const filterExpenseHandler = selectedYear => {
+    setFilteredYear(selectedYear)
+    console.log({filteredYear});
+  };
+
+
+  return (
+    <div>
+      <ExpensesFilter onChangeFilter={filterExpenseHandler} selected={filteredYear}/>
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
